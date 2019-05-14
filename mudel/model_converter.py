4e3837af0e68687e035965058cb3cd6ed78db54a
Andry <turbaants@gmail.com>
2019-05-14 18:51:28 +0300

    Update model_converter.py

--- mudel/model_converter.py
+++ mudel/model_converter.py
@@ -1,3 +1,8 @@
+#############################################################################################
+# kood pärineb selleltl lehelt:                                                             #
+# https://stackoverflow.com/questions/53256877/how-to-convert-kerash5-file-to-a-tflite-file #
+#############################################################################################
+
 import tensorflow as tf
 converter = tf.lite.TFLiteConverter.from_keras_model_file('.\model.h5' )
 tflite_model = converter.convert()
